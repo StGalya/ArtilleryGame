02c051fa62fdcc95c747ac1121769bbc13926983
AndreyTyshevich <43672780+AndreyTyshevich@users.noreply.github.com>
2018-12-07 13:08:20 +0300

    Update game.py
    Создан общий холст

--- game.py
+++ game.py
@@ -1,5 +1,18 @@
 from shell import *
 from cannon import *
 from ground import *
+
+from tkinter import *
+
+
 def start_game():
-    pass
+    root = Tk()
+    frame = Frame(root)
+    root.geometry('1200x700')
+    canv = Canvas(root, bg='white')
+    canv.pack(fill=BOTH, expand=1)
+
+    ground = Ground(canv)
+    ground.draw()
+
+    mainloop()
